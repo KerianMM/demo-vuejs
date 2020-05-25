2b0d96693f7a2af05b3fbc8f3ad187c258e50688
KerianMM <41105539+KerianMM@users.noreply.github.com>
2020-05-25 15:21:24 +0200

    add bootstrap vue
    Utilisation de composants bootstrap vue

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -3,19 +3,52 @@
     <div v-if="!images || !images.length">
       <span>Aucune image chargée</span>
     </div>
-    <div v-for="image in images" :key="image.id" style="width:40%; padding: 1rem">
-      <img :src="image.url" style="width:100%;">
+    <div v-else>
+      <b-carousel
+        id="images-carousel"
+        v-model="slide"
+        :interval="4000"
+        controls
+        indicators
+        fade
+        @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd"
+      >
+        <b-carousel-slide
+          v-for="image in images"
+          :key="image.id"
+          :caption="image.id"
+          :img-src="image.url"
+        />
+      </b-carousel>
     </div>
   </div>
 </template>
 
 <script>
+import { BCarousel, BCarouselSlide } from "bootstrap-vue";
+
 export default {
   name: "Images",
+  components: { BCarousel, BCarouselSlide },
   props: {
     images: {
       type: Array,
       required: true
+    }
+  },
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
     }
   }
 };
